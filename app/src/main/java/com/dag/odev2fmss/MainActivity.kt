176a40c83d6cf18f55570558b2eb4a370761473e
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-04 20:24:41 +0300

    Implemented Binding

--- app/src/main/java/com/dag/odev2fmss/MainActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.dag.odev2fmss
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dag.odev2fmss.databinding.ActivityMainBinding
 
+/**
+ * MainActivity --> LoginActivity
+ *
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.joinButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
